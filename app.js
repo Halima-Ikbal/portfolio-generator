e6f1c156d8e91337b0f88213d0f54c0d7abf66f7
Halima-Ikbal <100046854+Halima-Ikbal@users.noreply.github.com>
2022-06-23 02:47:14 -0500

    add html and page-template

---
+++ app.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+const profileDataArgs = process.argv.slice(2);
+console.log(profileDataArgs);
+
+const [name, github] = profileDataArgs;
+
+
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
